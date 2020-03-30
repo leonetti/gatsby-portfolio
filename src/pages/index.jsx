e0dd304cd79bccf4259f25533bfd2941f7824af8
Alexander Leonetti <alexleonetti.dev@gmail.com>
2020-03-29 19:33:34 -0700

    Added Layout for pages
    Includes blank header and footer components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import Layout from '../components/layout';
 import Blogs from '../components/blogs';
 
 const IndexPage = () => (
-  <>
+  <Layout>
     <div>Hello Alexander!</div>
     <Blogs />
-  </>
+  </Layout>
 );
 
 export default IndexPage;
