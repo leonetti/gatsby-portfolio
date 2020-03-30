2758d61488340a85c1c92b479759380a72a043ee
Alexander Leonetti <alexleonetti.dev@gmail.com>
2020-03-30 16:55:25 -0700

    Hooked up contentful cms to application

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import shortId from 'shortid';
+import PropTypes from 'prop-types';
 
 import landingStyles from '../styles/landing.module.scss';
 
-function Landing() {
-  return (
-    <div className={landingStyles.landing}>
-      <h1>Landing</h1>
-    </div>
-  );
-}
+const Landing = ({ data }) => (
+  <div className={landingStyles.landing}>
+    <span>Hello...</span>
+    <h1>{`I'm ${data.name}.`}</h1>
+    <h1>{`I'm a ${data.designation}.`}</h1>
+    <ul className="sub-data">
+      {data.bannerList.map((item) => <li key={shortId.generate()}>{item}</li>)}
+    </ul>
+  </div>
+);
+
+Landing.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string,
+    designation: PropTypes.string,
+    bannerList: PropTypes.array,
+  }).isRequired,
+};
 
 export default Landing;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import Landing from '../components/landing';
+import About from '../components/about';
 import Blogs from '../components/blogs';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout header="home">
-    <Landing />
+    <Landing
+      data={{
+        name: data.contentfulAboutMe.name,
+        designation: data.contentfulAboutMe.designation,
+        bannerList: data.contentfulAboutMe.bannerList,
+      }}
+    />
+    <About />
     <Blogs />
   </Layout>
 );
 
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    contentfulAboutMe: PropTypes.object,
+  }),
+};
+
+IndexPage.defaultProps = {
+  data: {
+    contentfulAboutMe: {},
+  },
+};
+
 export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    contentfulAboutMe {
+      name
+      designation
+      bannerList
+    }
+  }
+`;
