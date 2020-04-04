34681517cce6ab720c2080c9ca5d6c8d25772b66
Alexander Leonetti <alexanderleonetti@Alexanders-MacBook-Pro.local>
2020-04-04 12:34:00 -0700

    Added using-mouse class to remove outlines
    Kept outlines for tabbing users

---
+++ src/html.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            document.body.addEventListener('mousedown', function() {
+              document.body.classList.add('using-mouse');
+            });
+            
+            // Re-enable focus styling when Tab is pressed
+            document.body.addEventListener('keydown', function(event) {
+              if (event.keyCode === 9) {
+                document.body.classList.remove('using-mouse');
+              }
+            });
+        `,
+          }}
+        />
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
