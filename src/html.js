9ec2787280ed1115b37000868cc203038a1d9d90
Alexander Leonetti <alexanderleonetti@Alexanders-MacBook-Pro.local>
2020-04-09 12:52:14 -0700

    Updated meta viewport

--- src/html.js
+++ src/html.js
@@ -9,7 +9,7 @@ export default function HTML(props) {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no maximum-scale=1, user-scalable=0"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no"
         />
         {props.headComponents}
       </head>
