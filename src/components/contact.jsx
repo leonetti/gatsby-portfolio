460e3a6123dd2fc22503bedd98997d223c8cad78
Alexander Leonetti <alexanderleonetti@Alexanders-MacBook-Pro.local>
2020-04-09 15:57:43 -0700

    Adding Form Component Back

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,36 +8,6 @@ const Contact = () => (
       <h2 className={contactStyles.heading}>Contact</h2>
       <form
         name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        className={contactStyles.form}
-      >
-        {/* <input type="hidden" name="form-name" value="contact" /> */}
-        <p className={contactStyles.hidden}>
-          <label htmlFor="bot-field">
-            Don’t fill this out if you&#39;re human:
-            <input type="hidden" name="bot-field" />
-          </label>
-        </p>
-        <div className={contactStyles.form__row}>
-          <label className={contactStyles.form__label} htmlFor="name">
-            Name
-            <input className={contactStyles.text__input} id="name" name="name" placeholder="John Doe" type="text" />
-
-          </label>
-        </div>
-        <div className={contactStyles.form__row}>
-          <button
-            className={contactStyles.submit}
-            type="submit"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-      {/* <form
-        name="contact"
         method="POST"
         netlify-honeypot="bot-field"
         data-netlify="true"
@@ -70,7 +40,7 @@ const Contact = () => (
         <p>
           <button type="submit">Send</button>
         </p>
-      </form> */}
+      </form>
     </section>
   </div>
 );
